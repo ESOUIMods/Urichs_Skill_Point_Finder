6f44453036471e61c6290c14b8b58fc21c7b92de
Sharlikran <danielhmpdx16@gmail.com>
2021-05-14 00:09:04 -0700

    Version: 5.6.1

--- USPF.lua
+++ USPF.lua
@@ -874,7 +874,7 @@ end
 local enchantingSkillType, enchantingSkillLine = USPF_GetCraftingSkillLineIndices(CRAFTING_TYPE_ENCHANTING)
 local provisionSkillType, provisionSkillLine = USPF_GetCraftingSkillLineIndices(CRAFTING_TYPE_PROVISIONING)
 local DARK_BROTHERHOOD, FIGHTERS_GUILD, MAGES_GUILD, THIEVES_GUILD, UNDAUNTED, SOUL_MAGIC, LEGERDEMAIN, PSIJIC_ORDER = 118, 45, 44, 117, 55, 72, 111, 130
-local VAMPIRE, SCRYING, EXCAVATION = 51, 155, 157
+local VAMPIRE, SCRYING, EXCAVATION, WEREWOLF = 51, 155, 157, 50
 
 local function USPF_IsValidRacialLine(skillType, skillLine)
 	if (skillType == SKILL_TYPE_RACIAL) then
@@ -894,14 +894,19 @@ local function USPF_GetReduceAbility(skillType, skillLine, skillIndex)
 		val = ((skillLineId == EXCAVATION and skillIndex == 1) and 1 or val) -- Excavation - Hand Brush
 		val = ((skillLineId == EXCAVATION and skillIndex == 2) and 1 or val) -- Excavation - Augur
 		val = ((skillLineId == VAMPIRE and skillIndex == 7) and 1 or val) -- Vampire - Feed
-		val = ((IsWerewolfSkillLine(skillType, skillLine) and skillIndex == 1) and 1 or val) -- Werewolf - Transformation
-		val = ((IsWerewolfSkillLine(skillType, skillLine) and skillIndex == 7) and 1 or val) -- Werewolf - Devour
+		val = ((skillLineId == WEREWOLF and skillIndex == 1) and 1 or val) -- Werewolf - Transformation
+    val = ((skillLineId == WEREWOLF and skillIndex == 7) and 1 or val) -- Werewolf - Devour
 	end
 	if skillType == SKILL_TYPE_TRADESKILL then
 		val = (skillIndex == 1 and 1 or val)
 		val = ((skillIndex == 2 and skillType == enchantingSkillType and skillLine == enchantingSkillLine) and 1 or val)
 		val = ((skillIndex == 2 and skillType == provisionSkillType and skillLine == provisionSkillLine) and 1 or val)
 	end
+  if skillType == SKILL_TYPE_ARMOR then -- first armor passive is now free, as is the second for light and heavy armor
+    val = (skillIndex == 2 and 1 or val)
+    val = ((skillIndex == 3 and skillLine == ARMORTYPE_LIGHT) and 1 or val)
+    val = ((skillIndex == 3 and skillLine == ARMORTYPE_HEAVY) and 1 or val)
+  end
 
 	val = ((USPF_IsValidRacialLine(skillType, skillLine) and skillIndex == 1) and 1 or val)
 
