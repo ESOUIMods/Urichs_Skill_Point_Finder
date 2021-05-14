c202d5e170eacc7ee8fb7bd2d9b4c6b13262f434
Sharlikran <danielhmpdx16@gmail.com>
2021-05-14 00:02:38 -0700

    Version: 5.6.0

--- lang/strings.lua
+++ lang/strings.lua
@@ -1,24 +1,24 @@
 local strings = {
 	USPF_SETTINGS_FONT_TITLE = "Font Settings",
-	
+
 	USPF_SETTINGS_FONT_TITLE_HEADER = "Title/Footer Font",
 	USPF_SETTINGS_FONT_TITLE_DESC = "Set the table header and footer fonts.",
-	
+
 	USPF_SETTINGS_FONT_GSP_TITLE = "General Skill Points Font",
 	USPF_SETTINGS_FONT_GSP_ROWS = "Set the General Skill Points table row font.",
-	
+
 	USPF_SETTINGS_FONT_SQS_TITLE = "Quests & Skyshards Font",
 	USPF_SETTINGS_FONT_SQS_ROWS = "Set the Zone Quests & Skyshards table row font.",
-	
+
 	USPF_SETTINGS_FONT_GDQ_TITLE = "Group Dungeon Quests Font",
 	USPF_SETTINGS_FONT_GDQ_ROWS = "Set the Group Dungeon Quests table row font.",
-	
+
 	USPF_SETTINGS_FONT_PDB_TITLE = "Public Dungeon Group Events Font",
 	USPF_SETTINGS_FONT_PDB_ROWS = "Set the Public Dungeon Group Boss Events table row font.",
-	
+
 	USPF_SETTINGS_RELOAD_WARNING = "Will need to reload the UI.",
-	
-	
+
+
 	USPF_SETTINGS_COLOR_TITLE = "Color Settings",
 	USPF_SETTINGS_COLOR_GSP_DONE = "General - Done",
 	USPF_SETTINGS_COLOR_GSP_PROG = "General - In Progress",
@@ -36,8 +36,8 @@ local strings = {
 	USPF_SETTINGS_COLOR_DESC_DONE = "Set the color for complete skill points.",
 	USPF_SETTINGS_COLOR_DESC_PROG = "Set the color for incomplete, started skill points.",
 	USPF_SETTINGS_COLOR_DESC_NOT_DONE = "Set the color for incomplete skill points.",
-	
-	
+
+
 	USPF_SETTINGS_SORT_TITLE = "Sort Option Settings",
 	USPF_SETTINGS_SORT_SQS = "Quest and Skyshard Sorting",
 	USPF_SETTINGS_SORT_SQS_DESC = "Set the sort order for the Storyline Quests and Skyshards table.",
@@ -45,7 +45,7 @@ local strings = {
 	USPF_SETTINGS_SORT_GDQ_DESC = "Set the sort order for the Group Dungeon Quests table.",
 	USPF_SETTINGS_SORT_PDB = "Public Dungeon Sorting",
 	USPF_SETTINGS_SORT_PDB_DESC = "Set the sort order for the Public Dungeon Group Boss Events table.",
-	
+
 	USPF_SETTINGS_OVERRIDE_TITLE = "Override Settings",
 	USPF_SETTINGS_OVERRIDE_FOLIUM = "Folium Discognitum Override",
 	USPF_SETTINGS_OVERRIDE_FOLIUM_DESC = "This setting allows you to override the USPF's built-in Folium Discognitum logic. This is usually only necessary if you've not received skill points that you've actually earned.",
@@ -53,23 +53,23 @@ local strings = {
 	USPF_SETTINGS_OVERRIDE_FOLIUM_SET = "Character Has Folium Discognitum",
 	USPF_SETTINGS_OVERRIDE_FOLIUM_SET_DESC = "This setting forces the USPF's built-in Folium Discognitum points. You must select the Folium Discognitum Override option for this to have any effect.",
 	USPF_SETTINGS_OVERRIDE_FOLIUM_SET_WARN = "May result in an incorrect total amount of skill points in USPF.",
-	
+
 	USPF_SETTINGS_OVERRIDE_MWC_SET = "Morrowind Quest Override",
 	USPF_SETTINGS_OVERRIDE_MWC_SET_DESC = "This setting allows you to override the USPF's built-in Morrowind intro quest logic. This is usually only necessary if you've created a character after the release of Morrowind and skipped the \"Broken Bonds\" introduction quest.",
 	USPF_SETTINGS_OVERRIDE_MWC_SET_WARN = "May result in an incorrect total amount of skill points in USPF.",
-	
+
 	USPF_SETTINGS_OVERRIDE_SSC_SET = "Summerset Quest Override",
 	USPF_SETTINGS_OVERRIDE_SSC_SET_DESC = "This setting allows you to override the USPF's built-in Summerset intro quest logic. This is usually only necessary if you've created a character after the release of Summerset and skipped the \"The Mind Trap\" introduction quest.",
 	USPF_SETTINGS_OVERRIDE_SSC_SET_WARN = "May result in an incorrect total amount of skill points in USPF.",
-	
+
 	USPF_SETTINGS_OVERRIDE_EWC_SET = "Elsweyr Quest Override",
 	USPF_SETTINGS_OVERRIDE_EWC_SET_DESC = "This setting allows you to override the USPF's built-in Elsweyr intro quest logic. This is usually only necessary if you've created a character after the release of Elsweyr and skipped the \"Bright Moons, Warm Sands\" introduction quest.",
 	USPF_SETTINGS_OVERRIDE_EWC_SET_WARN = "May result in an incorrect total amount of skill points in USPF.",
-	
+
   USPF_SETTINGS_OVERRIDE_GMC_SET = "Greymoor Quest Override",
 	USPF_SETTINGS_OVERRIDE_GMC_SET_DESC = "This setting allows you to override the USPF's built-in Greymoor intro quest logic. This is usually only necessary if you've created a character after the release of Elsweyr and skipped the \"Bright Moons, Warm Sands\" introduction quest.",
 	USPF_SETTINGS_OVERRIDE_GMC_SET_WARN = "May result in an incorrect total amount of skill points in USPF.",
-  
+
 	USPF_GUI_CHAR_LEVEL	= "Character Level",
 	USPF_GUI_MAIN_QUEST	= "Main Quest",
 	USPF_GUI_FOLIUM		= "Folium Discognitum",
@@ -79,10 +79,10 @@ local strings = {
   USPF_GUI_GM_CHAR	= "Greymoor Character",
 	USPF_GUI_AVA_RANK	= "Alliance War Rank",
 	USPF_GUI_MAEL_ARENA	= "Maelstrom Arena",
-	
-	
+
+
 	USPF_GUI_TITLE		= "Urich's Skill Point Finder",
-	
+
 	USPF_GUI_GSP		= "General Skill Points",
 	USPF_GUI_SQS		= "Storyline Quests & Skyshards",
 	USPF_GUI_GDQ		= "Group Dungeon Quests",
@@ -95,30 +95,30 @@ local strings = {
 	USPF_GUI_GROUP_DUNGEON = "Group Dungeon",
 	USPF_GUI_PUBLIC_DUNGEON = "Public Dungeon",
 	USPF_GUI_DUNGEON_NAME = "Dungeon Name",
-	
+
 	USPF_GUI_TOTAL		= "Total",
 	USPF_GUI_CHAR_TOTAL	= "Character Total",
-	
-	
+
+
 	SI_BINDING_NAME_USPF_TOGGLE = "Show/Hide the USPF window.",
 	USPF_GUI_ZN_LCL		= "Lower",
 	USPF_GUI_ZN_UCL		= "Upper",
 	USPF_GUI_ZN_MQ		= "Main Quest",
-	
+
 	USPF_MSG_SHOW_GUI	= "USPF displayed.",
 	USPF_MSG_HIDE_GUI	= "USPF hidden.",
-	
+
 	USPF_MSG_BAD_SLASH	= "USPF invalid command.",
 	USPF_MSG_CMD_TITLE	= "USPF slash commands:",
 	USPF_MSG_CMD_OPTION	= "    /uspf - To %s the addon. This can be keybound.",
 	USPF_MSG_ACTIVATE	= "activate",
 	USPF_MSG_DEACTVATE	= "deactivate",
-	
+
 	USPF_MSG_HACK		= "I see you've been hacking my code. There's nothing to see here! Move along.",
-	
+
 	USPF_MSG_INIT		= "Running USPF for the first time!",
 	USPF_MSG_HELP		= "USPF Activated!",
-	
+
 	USPF_QUEST_NA		= "These skill points are not quest based.",
 	USPF_QUEST_NONE		= "There are no skill point quests in this zone.",
 	USPF_QUEST_AD1_1	= "Rites of the Queen",
@@ -229,6 +229,8 @@ local strings = {
 	USPF_QUEST_GD_UG	= "Unhallowed Grave",
 	USPF_QUEST_GD_SG	= "Method and Madness",
 	USPF_QUEST_GD_CT	= "Blood of the Past",
+	USPF_QUEST_GD_BDV	= "Burning Secrets",
+	USPF_QUEST_GD_TC	= "Into The Deep",
 	USPF_QUEST_IC		= "The Sublime Brazier",
 	USPF_QUEST_MM_1		= "Sunken Treasure",
 	USPF_QUEST_MM_2		= "Missing in Murkmire",
